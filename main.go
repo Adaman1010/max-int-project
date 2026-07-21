ff46f9cb2cbd6222e840849bd4da767a1ced101b
aybika <adamovaaybika@mail.ru>
2026-07-21 20:54:42 +0300

    Add workflow for new_workflow branch

--- main.go
+++ main.go
@@ -1,38 +1,7 @@
-// package main
-
-// func main() {
-
-// }
-
-// func MaxInt(a, b int) int {
-// 	if a >= b {
-// 		return a
-// 	}
-
-// 	return b
-// }
-
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(5)
 
-	i := 0
-
-	for ; i < 5; i++ {
-		go func() {
-			fmt.Println(i)
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
 }
 
 func MaxInt(a, b int) int {
